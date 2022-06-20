43f94ed689ca71e8de5bea1352927207ee94f41d
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-20 16:51:40 +0100

    Delete comments and clean code.

--- Mass Functions/match-mass.py
+++ Mass Functions/match-mass.py
@@ -28,6 +28,7 @@ def get_filenames_dark(sim_size, sim_res, num_files):
         i += 1
     return(filename)
 
+
 """
 Function that gets Subhalo Mass from given file list.
 """
@@ -53,59 +54,6 @@ def get_matching(sim_size, sim_res):
         matchingarr = np.array(file['Snapshot_99/SubhaloIndexDark_LHaloTree'])
         #print(matchingarr)
     return(matchingarr)
-'''
-"""
-Reading out the masses from the TNG files.
-
-"""
-filename = []
-mass = np.array([])
-i = 0
-# Making a list of all possible filenames
-while i < 11:
-    filename.append("/disk01/rmcg/downloaded/tng/tng50-4/fof_subfind_snapshot_99/fof_subhalo_tab_099."+str(i)+".hdf5")
-    i += 1
-#print(filename)
-g = 0
-
-# Getting mass for each file
-while g < len(filename):
-    with h5py.File(str(filename[g])) as file:
-        #print(file.keys())
-        submass = np.array(file['Subhalo/SubhaloMass'])
-        #print(submass)
-        mass = np.append(mass, submass)
-        g +=1
-print(str(mass[0]), str(mass[1]))
-print(len(mass))
-#print("Done")
-'''
-"""
-Reading out masses from dark files
-"""
-"""
-filenamedark = []
-massdark = np.array([])
-i = 0
-# Making a list of all possible filenames
-while i < 4:
-    filenamedark.append("/disk01/rmcg/downloaded/tng/tng50-4-dark/fof_subfind_snapshot_99/fof_subhalo_tab_099."+str(i)+".hdf5")
-    i += 1
-#print(filename)
-g = 0
-
-# Getting mass for each file
-while g < len(filenamedark):
-    with h5py.File(str(filenamedark[g])) as file:
-        #print(file.keys())
-        submass = np.array(file['Subhalo/SubhaloMass'])
-        #print(submass)
-        massdark = np.append(massdark, submass)
-        g +=1
-print(str(massdark[0]), str(massdark[1]))
-print(len(massdark))
-"""
-
 
 """
 Determining matching pairs and noting down their masses as corresponding data points
@@ -115,10 +63,7 @@ filenames_dark = get_filenames_dark(50, 1, 128)
 mass = get_mass(filenames_all)
 massdark = get_mass(filenames_dark)
 matchingarr = get_matching(50, 1)
-print(len(mass))
-print(len(massdark))
-print(len(matchingarr))
-print(matchingarr)
+
 i = 0
 orderdm = np.zeros(len(mass))
 while i<len(matchingarr):
