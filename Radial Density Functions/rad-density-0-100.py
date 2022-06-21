62736c464f3ffe59fd9c683b7fdac454361099dc
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-21 16:21:54 +0100

    Clean code, run for all halos 50-4

--- Radial Density Functions/rad-density-0-100.py
+++ Radial Density Functions/rad-density-0-100.py
@@ -186,7 +186,7 @@ filename_snap = get_filenames_snap(50, 4, 11)
 
 
 g = 0 
-while g < len(halfmassradii):    
+while g < 20:    
     particle_from_halo(g, pos[g], halfmassradii[g], (halfmassradii[g]*3), filename_snap)
     
 '''
