9f8fc5526b881b20efd758b2788b37b94551e953
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-22 12:00:54 +0100

    Check it works

--- Radial Density Functions/rad-density-func.py
+++ Radial Density Functions/rad-density-func.py
@@ -92,7 +92,8 @@ positions = get_pos(files)
 radius = get_rad(files)
 g = 0
 numhalos = 2
-rad_den = np.array([])
+densities = np.array([])
+radii = np.array([])
 while g < numhalos:
     print("hi")
     #with open('HaloParticles/50-1_snap_99_halo_'+str(g)+'_rad_mass_100kpc.csv', 'r') as datafile:
@@ -106,10 +107,12 @@ while g < numhalos:
     #print((data_transposed[0][1:].dtype, data_transposed[1][1:].dtype, data_transposed[2][1:].dtype,data_transposed[3][1:].dtype, interval.dtype, radius[g].dtype, positions[g][0].dtype, positions[g][1].dtype, positions[g][2].dtype))
     #print((data_transposed[0][1:].astype(float), data_transposed[1][1:].astype(float), data_transposed[2][1:].astype(float),data_transposed[3][1:].astype(float), interval, radius[g], positions[g][0], positions[g][1], positions[g][2]))
     rad_den = radial_density(data_csv['x'], data_csv['y'], data_csv['z'],data_csv['mass'], interval, radius[g], positions[g][0], positions[g][1], positions[g][2])
+    densities = np.append(densities, rad_den[0])
+    radii = np.append(radii, rad_den[1])
     print(rad_den)
     g += 1 
 
-print(rad_den)
+print(densities, radii)
     
 '''
     
