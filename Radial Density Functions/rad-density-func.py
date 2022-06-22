b8030ba5f6c69420529cec23a8881bcab47f5ead
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-22 11:22:13 +0100

    Fix data type

--- Radial Density Functions/rad-density-func.py
+++ Radial Density Functions/rad-density-func.py
@@ -98,8 +98,9 @@ while g < numhalos:
         #csvFile = csv.reader(datafile)
         #data_csv = list(csvFile)
         data_csv = pd.read_csv(datafile, dtype=np.float64)
-        data_transposed = np.array(data_csv).T
-        print((data_transposed[0][1:].dtype, data_transposed[1][1:].dtype, data_transposed[2][1:].dtype,data_transposed[3][1:].dtype, interval.dtype, radius[g].dtype, positions[g][0].dtype, positions[g][1].dtype, positions[g][2].dtype))
+        print(data_csv)
+        #data_transposed = np.array(data_csv).T
+        #print((data_transposed[0][1:].dtype, data_transposed[1][1:].dtype, data_transposed[2][1:].dtype,data_transposed[3][1:].dtype, interval.dtype, radius[g].dtype, positions[g][0].dtype, positions[g][1].dtype, positions[g][2].dtype))
         #print((data_transposed[0][1:].astype(float), data_transposed[1][1:].astype(float), data_transposed[2][1:].astype(float),data_transposed[3][1:].astype(float), interval, radius[g], positions[g][0], positions[g][1], positions[g][2]))
         #rad_den[g] = radial_density(data_transposed[0][1:].astype(float), data_transposed[1][1:].astype(float), data_transposed[2][1:].astype(float),data_transposed[3][1:].astype(float), interval, radius[g], positions[g][0], positions[g][1], positions[g][2])
     g += 1 
