bcf6dfcf5a8d77eebb1433c3d70b272962ad52f9
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-27 12:25:14 +0100

    Fit profiles

--- Radial Density Functions/new-bins_fit-profiles.py
+++ Radial Density Functions/new-bins_fit-profiles.py
@@ -238,24 +238,24 @@ print(np.shape(radii))
 print(np.shape(densities))
 print(np.shape(uncertainties))
 
-fitp, fitcov = scopt.curve_fit(nfw, radii, densities, p0=np.ones(2), sigma=uncertainties)
+fitp, fitcov = scopt.curve_fit(nfw, radii[0], densities[0], p0=np.ones(2), sigma=uncertainties[0])
 print ('Fitted value for NFW', fitp)
 print ('Uncertainties for NFW', np.sqrt(np.diag(fitcov)))
 
-fitp, fitcov = scopt.curve_fit(einasto, radii, densities, p0=np.ones(3), sigma=uncertainties)
+fitp, fitcov = scopt.curve_fit(einasto, radii[0], densities[0], p0=np.ones(3), sigma=uncertainties[0])
 print ('Fitted value for Einasto', fitp)
 print ('Uncertainties for Einasto', np.sqrt(np.diag(fitcov)))
 
-fitp, fitcov = scopt.curve_fit(burkert, radii, densities, p0=np.ones(2), sigma=uncertainties)
+fitp, fitcov = scopt.curve_fit(burkert,radii[0], densities[0], p0=np.ones(2), sigma=uncertainties[0])
 print ('Fitted value for Burkert', fitp)
 print ('Uncertainties for Burkert', np.sqrt(np.diag(fitcov)))
 
 
-fitp, fitcov = scopt.curve_fit(dehnen_twoparam, radii, densities, p0=np.ones(2), sigma=uncertainties)
+fitp, fitcov = scopt.curve_fit(dehnen_twoparam, radii[0], densities[0], p0=np.ones(2), sigma=uncertainties[0])
 print ('Fitted value for Dehnen Two Parameters', fitp)
 print ('Uncertainties for Dehnen Two Parameters', np.sqrt(np.diag(fitcov)))
 
-fitp, fitcov = scopt.curve_fit(dehnen_threeparam, radii, densities, p0=np.ones(3), sigma=uncertainties)
+fitp, fitcov = scopt.curve_fit(dehnen_threeparam, radii[0], densities[0], p0=np.ones(3), sigma=uncertainties[0])
 print ('Fitted value for Dehnen Three Parameters', fitp)
 print ('Uncertainties for Dehnen Three Parameters', np.sqrt(np.diag(fitcov)))
 
