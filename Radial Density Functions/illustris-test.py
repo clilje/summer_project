d0efb461d3320a507556c84b270114cc0ae2bf23
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-30 12:06:25 +0100

    New directory path defined

--- Radial Density Functions/illustris-test.py
+++ Radial Density Functions/illustris-test.py
@@ -8,11 +8,11 @@ import sys
 sys.path.insert(1, '/home/clilje/summer_project')
 from illustris_python import groupcat,lhalotree,snapshot,sublink,util
 #from localhost.Ubuntu.home.clara.summer_project import illustris_python as il
+sys.path.insert(1, '/')
 
-
-basePath = './disk01/rmcg/downloaded/tng/tng50-1'
+basePath = '/disk01/rmcg/downloaded/tng/tng50-1'
 fields = ['SubhaloMass','SubhaloSFRinRad']
-subhalos = groupcat.loadSubhalos(basePath,135,fields=fields)
+subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
 #tree = sublink.loadtre()
 subhalos.keys()
 subhalos['SubhaloMass'].shape
