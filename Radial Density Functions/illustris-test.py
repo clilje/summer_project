4dc578ab4f430ffbd0b7d9d563d1a3b1e8c35a34
clilje <C.Lilje@sms.ed.ac.uk>
2022-06-30 11:54:16 +0100

    New Result Table and installing illustris python package

---
+++ Radial Density Functions/illustris-test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 11:23:03 2022
+
+@author: clara
+"""
+import sys
+sys.path.insert(1, '//wsl.localhost/Ubuntu/home/clara/summer_project')
+from illustris_python import groupcat,lhalotree,snapshot,sublink,util
+#from localhost.Ubuntu.home.clara.summer_project import illustris_python as il
+
+
+basePath = './disk01/rmcg/downloaded/tng/tng50-1'
+fields = ['SubhaloMass','SubhaloSFRinRad']
+subhalos = groupcat.loadSubhalos(basePath,135,fields=fields)
+#tree = sublink.loadtre()
+subhalos.keys()
+subhalos['SubhaloMass'].shape
