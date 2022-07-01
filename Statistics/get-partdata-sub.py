7bad6b41a794475aaaa5798cb2ef86d25a4c0a7b
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-01 22:33:41 +0100

    Sub runs, determine best speed

---
+++ Statistics/get-partdata-sub.py
@@ -0,0 +1,105 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import csv
+from pathlib import Path
+import sys
+sys.path.insert(1, '/home/clilje/summer_project')
+from illustris_python import groupcat,lhalotree,snapshot,sublink,util
+
+size = 50
+res = 1
+dark = False
+#z = 0
+snapnum = 99
+
+if dark == True:
+    basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res)+'-dark'
+else:
+   basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res) 
+fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass']
+
+
+subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
+num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
+
+a = (groupcat.loadHeader(basePath, snapnum)['Time']).astype('int')  #scalefactor
+z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redshift
+x = 0
+while x <= (len(num_halo)):
+    if dark == False: 
+        filename = 'HaloParticles50-1-sub/snap_99_halo_'+str(x)+'.csv'
+    else: 
+        filename = 'HaloParticles50-1-sub/snap_99_halo_'+str(x)+'-dark.csv'
+    with open(filename, 'w', encoding='UTF8', newline='') as f:
+        print(x)
+        header = ['ID','Type','x','y','z','mass','vx','vy','vz']
+        # Create a writer object
+        fwriter = csv.writer(f, delimiter=',')
+        # Write the header
+        fwriter.writerow(header)
+        if dark == False:
+            gasparts = snapshot.loadHalo(basePath, snapnum, x, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+            starparts = snapshot.loadHalo(basePath, snapnum, x, 'stars', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+            bhparts = snapshot.loadHalo(basePath, snapnum, x, 'bh', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+        dmparts = snapshot.loadHalo(basePath, snapnum, x, 'dm', fields=['Coordinates','ParticleIDs','Velocities'])
+        
+        with h5py.File(snapshot.snapPath(basePath,snapnum),'r') as f:
+            header = dict( f['Header'].attrs.items() )
+            #print(header['MassTable'][1]) # 10^10 msun/h
+            dmmass = [(header['MassTable'][1]).astype('float')]*len(dmparts['ParticleIDs'])
+        
+        lowerbound = 0
+        indexjump = 999999
+        while lowerbound < len(gasparts['ParticleIDs']):
+            print(lowerbound)
+            if (lowerbound+indexjump)>len(gasparts['ParticleIDs']):
+                upperbound = len(gasparts['ParticleIDs'])
+            else:
+                upperbound = lowerbound+indexjump
+            if dark == False:
+                gasdata = np.vstack([gasparts['ParticleIDs'][lowerbound:upperbound],
+                                     ['gas']*len(gasparts['ParticleIDs'][lowerbound:upperbound]), 
+                                     gasparts['Coordinates'][:, 0][lowerbound:upperbound],
+                                     gasparts['Coordinates'][:, 1][lowerbound:upperbound],
+                                     gasparts['Coordinates'][:, 2][lowerbound:upperbound], 
+                                     gasparts['Masses'][lowerbound:upperbound], 
+                                     gasparts['Velocities'][:, 0][lowerbound:upperbound],
+                                     gasparts['Velocities'][:, 1][lowerbound:upperbound],
+                                     gasparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
+                fwriter.writerows(gasdata)
+                stardata = np.vstack([starparts['ParticleIDs'][lowerbound:upperbound],
+                                      ['stars']*len(starparts['ParticleIDs'][lowerbound:upperbound]), 
+                                      starparts['Coordinates'][:, 0][lowerbound:upperbound],
+                                      starparts['Coordinates'][:, 1][lowerbound:upperbound],
+                                      starparts['Coordinates'][:, 2][lowerbound:upperbound], 
+                                      starparts['Masses'][lowerbound:upperbound], 
+                                      starparts['Velocities'][:, 0][lowerbound:upperbound],
+                                      starparts['Velocities'][:, 1][lowerbound:upperbound],
+                                      starparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
+                fwriter.writerows(stardata)
+                bhdata = np.vstack([bhparts['ParticleIDs'][lowerbound:upperbound],
+                                    ['bh']*len(bhparts['ParticleIDs'][lowerbound:upperbound]), 
+                                    bhparts['Coordinates'][:, 0][lowerbound:upperbound],
+                                    bhparts['Coordinates'][:, 1][lowerbound:upperbound],
+                                    bhparts['Coordinates'][:, 2][lowerbound:upperbound], 
+                                    bhparts['Masses'][lowerbound:upperbound], 
+                                    bhparts['Velocities'][:, 0][lowerbound:upperbound],
+                                    bhparts['Velocities'][:, 1][lowerbound:upperbound],
+                                    bhparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
+                fwriter.writerows(bhdata)
+            dmdata = np.vstack([dmparts['ParticleIDs'][lowerbound:upperbound],
+                                ['dm']*len(dmparts['ParticleIDs'][lowerbound:upperbound]), 
+                                dmparts['Coordinates'][:, 0][lowerbound:upperbound],
+                                dmparts['Coordinates'][:, 1][lowerbound:upperbound],
+                                dmparts['Coordinates'][:, 2][lowerbound:upperbound], 
+                                dmmass[lowerbound:upperbound], 
+                                dmparts['Velocities'][:, 0][lowerbound:upperbound],
+                                dmparts['Velocities'][:, 1][lowerbound:upperbound],
+                                dmparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
+            fwriter.writerows(dmdata)
+            
+            lowerbound = upperbound
+    print(x)
+    x +=1'''
--- Statistics/trial-get-partdata.py
+++ Statistics/trial-get-partdata.py
@@ -35,24 +35,14 @@ z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redsh
 print(round(time.time()-tt,2))
 tt = time.time()
 #print(groupcat.loadHeader(basePath, snapnum))
-"""
-with open('50-1-subhalo-info.csv', 'w', encoding='UTF8', newline='') as subfile:
-    header = ['SubhaloIndex','SubhaloPosX','SubhaloPosY','SubhaloPosZ','SubhaloHalfmassRad','SubhaloMass']
-    fwriter = csv.writer(subfile, delimiter=',')
-    # Write the header
-    fwriter.writerow(header)
-    data = np.vstack([num_halo, subhalos['SubhaloPos'][:, 0],subhalos['SubhaloPos'][:, 1],subhalos['SubhaloPos'][:, 2], subhalos['SubhaloHalfmassRad'], subhalos['SubhaloMass']]).transpose()
-    fwriter.writerows(data)
-"""
-#gasparts = snapshot.loadHalo(basePath, snapnum, 0, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
-#print(gasparts['Coordinates'])
+
 x = 0
 pdheader = ['ID','Type','x','y','z','mass','vx','vy','vz']
 while x <= (len(num_halo)):
     if dark == False: 
-        filename = 'HaloParticles50-1/snap_99_halo_'+str(x)
+        filename = 'HaloParticles50-1-pd/snap_99_halo_'+str(x)
     else: 
-        filename = 'HaloParticles50-1/snap_99_halo_'+str(x)+'-dark'
+        filename = 'HaloParticles50-1-pd/snap_99_halo_'+str(x)+'-dark'
     #with open(filename, 'w', encoding='UTF8', newline='') as f:
     
     print(round(time.time()-tt,2))
@@ -74,13 +64,13 @@ while x <= (len(num_halo)):
         dmmass = [(header['MassTable'][1]).astype('float')]*len(dmparts['ParticleIDs'])
     
     lowerbound = 0
-    indexjump = int(len(gasparts['ParticleIDs'])/300)
+    indexjump = 900000
     
     derek = pd.DataFrame(columns=pdheader)
     
     while lowerbound < len(gasparts['ParticleIDs']):
         
-        print(lowerbound)
+        #print(lowerbound)
         
         miniderek = pd.DataFrame(columns=pdheader)
         if (lowerbound+indexjump)>len(gasparts['ParticleIDs']):
@@ -89,7 +79,7 @@ while x <= (len(num_halo)):
             upperbound = lowerbound+indexjump
         
         if dark == False:
-            
+            #Gas Particles
             miniderek['ID']=gasparts['ParticleIDs'][lowerbound:upperbound]
             miniderek['Type']=['gas']*len(gasparts['ParticleIDs'][lowerbound:upperbound])
             miniderek['x']=gasparts['Coordinates'][:, 0][lowerbound:upperbound]
@@ -101,22 +91,12 @@ while x <= (len(num_halo)):
             miniderek['vz']=gasparts['Velocities'][:, 2][lowerbound:upperbound]
             
             
-            print(miniderek)
+            #print(miniderek)
             
-            """
-            gasdata = np.vstack([gasparts['ParticleIDs'][lowerbound:upperbound],
-                                 ['gas']*len(gasparts['ParticleIDs'][lowerbound:upperbound]), 
-                                 gasparts['Coordinates'][:, 0][lowerbound:upperbound],
-                                 gasparts['Coordinates'][:, 1][lowerbound:upperbound],
-                                 gasparts['Coordinates'][:, 2][lowerbound:upperbound], 
-                                 gasparts['Masses'][lowerbound:upperbound], 
-                                 gasparts['Velocities'][:, 0][lowerbound:upperbound],
-                                 gasparts['Velocities'][:, 1][lowerbound:upperbound],
-                                 gasparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
-            """
             derek = pd.concat([derek,miniderek])
             miniderek = miniderek[0:0]
             
+            #Star Particles
             miniderek['ID']=starparts['ParticleIDs'][lowerbound:upperbound]
             miniderek['Type']=['star']*len(starparts['ParticleIDs'][lowerbound:upperbound])
             miniderek['x']=starparts['Coordinates'][:, 0][lowerbound:upperbound]
@@ -129,20 +109,8 @@ while x <= (len(num_halo)):
             
             derek = pd.concat([derek,miniderek])
             miniderek = miniderek[0:0]
-            
-            """
-            #fwriter.writerows(gasdata)
-            stardata = np.vstack([starparts['ParticleIDs'][lowerbound:upperbound],
-                                  ['stars']*len(starparts['ParticleIDs'][lowerbound:upperbound]), 
-                                  starparts['Coordinates'][:, 0][lowerbound:upperbound],
-                                  starparts['Coordinates'][:, 1][lowerbound:upperbound],
-                                  starparts['Coordinates'][:, 2][lowerbound:upperbound], 
-                                  starparts['Masses'][lowerbound:upperbound], 
-                                  starparts['Velocities'][:, 0][lowerbound:upperbound],
-                                  starparts['Velocities'][:, 1][lowerbound:upperbound],
-                                  starparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
-            fwriter.writerows(stardata)
-            """
+
+            #Black Holes
             miniderek['ID']=bhparts['ParticleIDs'][lowerbound:upperbound]
             miniderek['Type']=['bh']*len(bhparts['ParticleIDs'][lowerbound:upperbound])
             miniderek['x']=bhparts['Coordinates'][:, 0][lowerbound:upperbound]
@@ -154,18 +122,8 @@ while x <= (len(num_halo)):
             miniderek['vz']=bhparts['Velocities'][:, 2][lowerbound:upperbound]
             derek = pd.concat([derek,miniderek])
             miniderek =miniderek[0:0]
-            '''
-            bhdata = np.vstack([bhparts['ParticleIDs'][lowerbound:upperbound],
-                                ['bh']*len(bhparts['ParticleIDs'][lowerbound:upperbound]), 
-                                bhparts['Coordinates'][:, 0][lowerbound:upperbound],
-                                bhparts['Coordinates'][:, 1][lowerbound:upperbound],
-                                bhparts['Coordinates'][:, 2][lowerbound:upperbound], 
-                                bhparts['Masses'][lowerbound:upperbound], 
-                                bhparts['Velocities'][:, 0][lowerbound:upperbound],
-                                bhparts['Velocities'][:, 1][lowerbound:upperbound],
-                                bhparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
-            fwriter.writerows(bhdata)
-            '''
+
+        #DM Particles
         miniderek['ID']=dmparts['ParticleIDs'][lowerbound:upperbound]
         miniderek['Type']=['dm']*len(dmparts['ParticleIDs'][lowerbound:upperbound])
         miniderek['x']=dmparts['Coordinates'][:, 0][lowerbound:upperbound]
@@ -177,19 +135,9 @@ while x <= (len(num_halo)):
         miniderek['vz']=dmparts['Velocities'][:, 2][lowerbound:upperbound]
         derek = pd.concat([derek,miniderek])
         miniderek = miniderek[0:0]
-        """
-        dmdata = np.vstack([dmparts['ParticleIDs'][lowerbound:upperbound],
-                            ['dm']*len(dmparts['ParticleIDs'][lowerbound:upperbound]), 
-                            dmparts['Coordinates'][:, 0][lowerbound:upperbound],
-                            dmparts['Coordinates'][:, 1][lowerbound:upperbound],
-                            dmparts['Coordinates'][:, 2][lowerbound:upperbound], 
-                            dmmass[lowerbound:upperbound], 
-                            dmparts['Velocities'][:, 0][lowerbound:upperbound],
-                            dmparts['Velocities'][:, 1][lowerbound:upperbound],
-                            dmparts['Velocities'][:, 2][lowerbound:upperbound]]).transpose()
-        fwriter.writerows(dmdata)
-        """
+
         lowerbound = upperbound
+        #derek = derek.reset_index(drop=True)
         derek.to_csv(filename+'.csv', mode='a')
         #derek.to_hdf(filename+'.hdf', mode='a')
         derek = derek[0:0]
