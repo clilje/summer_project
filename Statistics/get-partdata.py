e6354b983fdc47f614bcf018a86f50ac169e2ddd
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-01 10:43:08 +0100

    Fix bug and only redo individual Halo particle recovery

--- Statistics/get-partdata.py
+++ Statistics/get-partdata.py
@@ -1,16 +1,3 @@
-"""import numpy as np
-a = np.array([1, 2, 3, 4])
-
-b = np.array([5, 6, 7, 8])
-
-c = np.array([2, 3, 4, 5])
-
-d = np.array([6, 8, 2, 4])
-
-e = np.vstack([a, b, c, d]).transpose()
-print(e)
-"""
-
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,7 +11,7 @@ from illustris_python import groupcat,lhalotree,snapshot,sublink,util
 size = 50
 res = 1
 dark = False
-z = 0
+#z = 0
 snapnum = 99
 
 if dark == True:
@@ -32,9 +19,15 @@ if dark == True:
 else:
    basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res) 
 fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass']
+
+
 subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
 num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
 
+a = (groupcat.loadHeader(basePath, snapnum)['Time']).astype('int')  #scalefactor
+z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redshift
+
+"""
 with open('50-1-subhalo-info.csv', 'w', encoding='UTF8', newline='') as subfile:
     header = ['SubhaloIndex','SubhaloPosX','SubhaloPosY','SubhaloPosZ','SubhaloHalfmassRad','SubhaloMass']
     fwriter = csv.writer(subfile, delimiter=',')
@@ -42,10 +35,10 @@ with open('50-1-subhalo-info.csv', 'w', encoding='UTF8', newline='') as subfile:
     fwriter.writerow(header)
     data = np.vstack([num_halo, subhalos['SubhaloPos'][:, 0],subhalos['SubhaloPos'][:, 1],subhalos['SubhaloPos'][:, 2], subhalos['SubhaloHalfmassRad'], subhalos['SubhaloMass']]).transpose()
     fwriter.writerows(data)
-    
+"""    
 
-gasparts = snapshot.loadHalo(basePath, snapnum, 0, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
-print(gasparts['Coordinates'])
+#gasparts = snapshot.loadHalo(basePath, snapnum, 0, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+#print(gasparts['Coordinates'])
 x = 0
 while x <= (len(num_halo)):
     with open('HaloParticles50-1/snap_99_halo_'+str(x)+'.csv', 'w', encoding='UTF8', newline='') as f:
@@ -58,7 +51,7 @@ while x <= (len(num_halo)):
             gasparts = snapshot.loadHalo(basePath, snapnum, x, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
             starparts = snapshot.loadHalo(basePath, snapnum, x, 'stars', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
             bhparts = snapshot.loadHalo(basePath, snapnum, x, 'bh', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
-        dmparts = snapshot.loadHalo(basePath, snapnum, x, 'dm', fields=['Coordinates','ParticleIDs','Masses'])
+        dmparts = snapshot.loadHalo(basePath, snapnum, x, 'dm', fields=['Coordinates','ParticleIDs','Velocities'])
         dmmass = [(groupcat.loadHeader(basePath, snapnum)['MassTable'][1]).astype('float')]*len(dmparts['ParticleIDs'])
         
 
@@ -68,7 +61,7 @@ while x <= (len(num_halo)):
         fwriter.writerows(stardata)
         bhdata = np.vstack([bhparts['ParticleIDs'],['bh']*len(bhparts['ParticleIDs']), bhparts['Coordinates'][:, 0],bhparts['Coordinates'][:, 1],bhparts['Coordinates'][:, 2], bhparts['Masses'], bhparts['Velocities'][:, 0],bhparts['Velocities'][:, 1],bhparts['Velocities'][:, 2]]).transpose()
         fwriter.writerows(bhdata)
-        dmdata = np.vstack([dmparts['ParticleIDs'],['dm']*len(dmparts['ParticleIDs']), dmparts['Coordinates'][:, 0],dmparts['Coordinates'][:, 1],dmparts['Coordinates'][:, 2], dmparts['Masses'], dmparts['Velocities'][:, 0],dmparts['Velocities'][:, 1],dmparts['Velocities'][:, 2]]).transpose()
+        dmdata = np.vstack([dmparts['ParticleIDs'],['dm']*len(dmparts['ParticleIDs']), dmparts['Coordinates'][:, 0],dmparts['Coordinates'][:, 1],dmparts['Coordinates'][:, 2], dmmass, dmparts['Velocities'][:, 0],dmparts['Velocities'][:, 1],dmparts['Velocities'][:, 2]]).transpose()
         fwriter.writerows(dmdata)
     print(x)
     x +=1
