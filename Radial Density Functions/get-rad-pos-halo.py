032b70a55e150c3b9cfd51e9960590c7d5d91868
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-01 15:30:03 +0100

    For local plotting

---
+++ Radial Density Functions/get-rad-pos-halo.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 14:38:14 2022
+
+@author: clara
+"""
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import csv
+import pandas as pd
+import scipy.optimize as scopt
+import scipy.linalg
+import scipy.stats
+
+h = 0.6774
+
+
+def get_filenames(sim_size, sim_res, num_files):
+    """
+    
+
+    Parameters
+    ----------
+    sim_size : Size of Edge of simulation box in kpc.
+    sim_res : Resolution of simulation, integer from 1 to 4
+    num_files : number of files in given simulation folder
+
+    Returns
+    -------
+    List of filnames in string formats
+
+    """
+    filename = []
+    i = 0
+    # Making a list of all possible filenames
+    while i < num_files:
+        filename.append("/disk01/rmcg/downloaded/tng/tng"+str(sim_size)+"-"+str(sim_res)+"/fof_subfind_snapshot_99/fof_subhalo_tab_099."+str(i)+".hdf5")
+        i += 1
+    return(filename)
+
+
+def get_pos(filename):
+    """
+    
+
+    Parameters
+    ----------
+    filename : Array of group file names in string format from which to extract the subhalo position.
+
+    Returns
+    -------
+    Array of arrays containing x,y,z position of subhalo centres.
+
+    """
+    g = 0
+    pos = np.array([])
+    while g < len(filename):
+        with h5py.File(str(filename[g])) as file:
+            if 'Subhalo/SubhaloPos'in file:
+                #print(file['Subhalo'])
+                subpos = np.array(file['Subhalo/SubhaloPos'])
+                pos = np.append(pos, subpos)
+            g +=1
+        #print(pos)
+    pos = np.reshape(pos, [int(len(pos)/3),3])
+    return(pos)
+
+def get_rad(filename):
+    """
+    
+
+    Parameters
+    ----------
+    filename : The names of the group files in which to look for halfmass radii.
+
+    Returns
+    -------
+    Array containing the halfmass radius for each Subhalo in the order they are saved in the files by TNG.
+
+    """
+    g = 0
+    rad = np.array([])
+    while g < len(filename):
+        with h5py.File(str(filename[g])) as file:
+            if 'Subhalo/SubhaloHalfmassRad'in file:
+                #print(file['Subhalo'])
+                subrad = np.array(file['Subhalo/SubhaloHalfmassRad'])
+                rad = np.append(rad, subrad)
+            g +=1
+        #pos = np.reshape(pos, [int(len(pos)/3),3])
+    return(rad)
+
+
+#interval = np.logspace(0.1, 2.5, 100)
+files = get_filenames(50, 4, 11)
+positions = get_pos(files)
+radius = get_rad(files)
+
+with open('HaloFits/50-4_halodata.csv', 'w', encoding='UTF8', newline='') as f:
+    
+    header = ['HaloPosX','HaloPosY','HaloPosZ','HaloHMrad']
+    # Create a writer object
+    fwriter = csv.writer(f, delimiter=',')
+    # Write the header
+    fwriter.writerow(header)
+    data = gasdata = np.vstack([positions[:, 0],positions[:, 1], positions[:, 2],radius]).transpose()
+    fwriter.writerow(data)
+        
+
