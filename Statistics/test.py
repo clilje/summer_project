b18e7903534b88548672d4d19de3f344816eff62
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-05 11:23:31 +0100

    Two test halos

--- Statistics/test.py
+++ Statistics/test.py
@@ -40,6 +40,30 @@ def distancefromcentre(cx, cy, cz, x, y, z, ):
 
 
 
+x = 49
+pdheader = ['ID','Type','x','y','z','mass','vx','vy','vz']
+filename = 'HaloParticles50-1-pd/snap_99_halo_'+str(x)
+snapnum = 99
+basePath = '/disk01/rmcg/downloaded/tng/tng50-1'
+fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass','SubhaloLen']
+
+
+subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
+num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
+#data = np.vstack([num_halo, subhalos['SubhaloPos'][:, 0],subhalos['SubhaloPos'][:, 1],subhalos['SubhaloPos'][:, 2], subhalos['SubhaloHalfmassRad'], subhalos['SubhaloMass']]).transpose()
+halo_50 = [subhalos['SubhaloPos'][x, 0],subhalos['SubhaloPos'][x, 1],subhalos['SubhaloPos'][x, 2], subhalos['SubhaloHalfmassRad'][x], subhalos['SubhaloMass'][x], subhalos['SubhaloLen'][x]]
+print(halo_50)
+
+gasparts = snapshot.loadHalo(basePath, snapnum, x, 'gas', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+starparts = snapshot.loadHalo(basePath, snapnum, x, 'stars', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+bhparts = snapshot.loadHalo(basePath, snapnum, x, 'bh', fields=['Coordinates','ParticleIDs','Velocities','Masses'])
+dmparts = snapshot.loadHalo(basePath, snapnum, x, 'dm', fields=['Coordinates','ParticleIDs','Velocities'])
+
+partx = np.append(gasparts['Coordinates'][:,0],[starparts['Coordinates'][:,0],bhparts['Coordinates'][:,0],dmparts['Coordinates'][:,0]])
+party = np.append(gasparts['Coordinates'][:,1],[starparts['Coordinates'][:,1],bhparts['Coordinates'][:,1],dmparts['Coordinates'][:,1]])
+partz = np.append(gasparts['Coordinates'][:,2],[starparts['Coordinates'][:,2],bhparts['Coordinates'][:,2],dmparts['Coordinates'][:,2]])
+
+print(len(partx))
 
 x = 50
 pdheader = ['ID','Type','x','y','z','mass','vx','vy','vz']
@@ -65,9 +89,9 @@ party = np.append(gasparts['Coordinates'][:,1],[starparts['Coordinates'][:,1],bh
 partz = np.append(gasparts['Coordinates'][:,2],[starparts['Coordinates'][:,2],bhparts['Coordinates'][:,2],dmparts['Coordinates'][:,2]])
 
 print(len(partx))
-dis = distancefromcentre(halo_50[0], halo_50[1], halo_50[2], partx, party, partz)
-print(dis)
-print(np.min(dis))
+#dis = distancefromcentre(halo_50[0], halo_50[1], halo_50[2], partx, party, partz)
+#print(dis)
+#print(np.min(dis))
 
 
 
@@ -95,6 +119,6 @@ party = np.append(gasparts['Coordinates'][:,1],[starparts['Coordinates'][:,1],bh
 partz = np.append(gasparts['Coordinates'][:,2],[starparts['Coordinates'][:,2],bhparts['Coordinates'][:,2],dmparts['Coordinates'][:,2]])
 
 print(len(partx))
-dis = distancefromcentre(halo_50[0], halo_50[1], halo_50[2], partx, party, partz)
-print(dis)
-print(np.min(dis))
+#dis = distancefromcentre(halo_50[0], halo_50[1], halo_50[2], partx, party, partz)
+#print(dis)
+#print(np.min(dis))
