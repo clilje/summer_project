95dc178c830d4800307a636a31dcfbaa5e7924e0
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-11 11:53:54 +0100

    play with scales and plotting

--- Statistics/heatmap-fits.py
+++ Statistics/heatmap-fits.py
@@ -5,6 +5,7 @@ Created on Thu Jun 16 14:38:14 2022
 @author: clara
 """
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.optimize as scopt
@@ -78,9 +79,9 @@ def plotting(rad, den, virial_radius, virial_density,nfwfitp,burkertfitp,dehnen_
     print(normalizeC)
     Z = Z/normalizeC
     print(Z)
-    Z[np.where(Z>10**100)[0]] = 10**100
+    Z[np.where(Z==np.inf)[0]] = 10**10
     print(Z)
-    pc = axs2.pcolor(X,Y,Z,plt.colors.LogNorm(vmin=Z.min(), vmax=Z.max()),cmap='PuBu_r', shading='auto')
+    pc = axs2.pcolor(X,Y,Z,matplotlib.colors.LogNorm(vmin=Z.min(), vmax=Z.max()),cmap='PuBu_r', shading='auto')
     #colorbar_scale = np.logspace(0,100,200)
     #axs2.colorbar()
     fig.colorbar(pc, ax=axs2, extend='max')
