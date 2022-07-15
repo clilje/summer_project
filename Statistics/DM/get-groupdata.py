eef769d985e3292d0f15b179e800597ef5d9878d
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-15 09:41:09 +0100

    Get group catalogue data fro DMO

---
+++ Statistics/DM/get-groupdata.py
@@ -0,0 +1,47 @@
+"""
+Created on Wed Jul  6 22:48:58 2022
+This file was actually used to create the group catalogue file of all halos.
+
+@author: clara
+"""
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import csv
+from pathlib import Path
+import sys
+sys.path.insert(1, '/home/clilje/summer_project')
+from illustris_python import groupcat,lhalotree,snapshot,sublink,util
+
+size = 50
+res = 1
+dark = True
+#z = 0
+snapnum = 99
+
+if dark == True:
+    basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res)+'-dark'
+else:
+   basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res) 
+fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass','SubhaloLen']
+
+
+subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
+num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
+
+a = (groupcat.loadHeader(basePath, snapnum)['Time']).astype('int')  #scalefactor
+z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redshift
+#print(a)
+#print(z)
+
+#print(groupcat.loadHeader(basePath, snapnum))
+
+with open('../50-1-subhalo-info-dark.csv', 'w', encoding='UTF8', newline='') as subfile:
+    header = ['SubhaloIndex','SubhaloPosX','SubhaloPosY','SubhaloPosZ','SubhaloHalfmassRad','SubhaloMass','SubhaloLen']
+    fwriter = csv.writer(subfile, delimiter=',')
+    # Write the header
+    fwriter.writerow(header)
+    data = np.vstack([num_halo, subhalos['SubhaloPos'][:, 0],subhalos['SubhaloPos'][:, 1],subhalos['SubhaloPos'][:, 2], subhalos['SubhaloHalfmassRad'], subhalos['SubhaloMass'], subhalos['SubhaloLen']]).transpose()
+    fwriter.writerows(data)
