eef50eedb9cc339ee05a00ca61fe4f74f17ae786
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-18 18:38:07 +0100

    Sklearn does not work on laptop lol

---
+++ ML/predict-dmmass.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 15:22:05 2022
+
+@author: clara
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import csv
+import pandas as pd
+#import scikit-learn as sklearn
+from sklearn.linear_model import LinearRegression
+
+h = 0.6774
+p_crit = 127 #m_sun/(kpc^3)
+
+data_csv = pd.read_csv('50-1-subhalo-info.csv')
+print(data_csv.columns.values)
+column_names = ['SubhaloGasMass', 'SubhaloStarMass','SubhaloBHMass']
+y = data_csv['SubhaloDMMass']
+X = data_csv[column_names]
+model = LinearRegression(fit_intercept=False)
+model.fit(X,y)
+data_csv['predicted'] = model.predict(X)
+plt.plot(y,data_csv['predicted'])
+plt.savefig('massprediction.jpg')
+plt.show()
