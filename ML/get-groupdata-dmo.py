ec8cbabd3a0bff9bce67c5a11ab235f7fc9a8733
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-22 17:13:40 +0100

    Get new info from group catalogue files

--- ML/get-groupdata-dmo.py
+++ ML/get-groupdata-dmo.py
@@ -15,6 +15,28 @@ import sys
 sys.path.insert(1, '/home/clilje/summer_project')
 from illustris_python import groupcat,lhalotree,snapshot,sublink,util
 
+def distancefromcentre(cx, cy, cz, x, y, z, ):
+    """
+    
+
+    Parameters
+    ----------
+    cx : Halo centre x-coord float
+    cy : Halo centre y-coord float
+    cz : Halo centre z-coord float
+    x : Particle Position x-coord
+    y : Particle Position y-coord
+    z : Particle Position z-coord
+
+    Returns
+    -------
+    Distance between particle and halo centre
+
+    """
+    return (np.sqrt((np.power(np.subtract(x,cx), 2)+ np.power(np.subtract(y,cy), 2) + np.power(np.subtract(z,cz), 2)))) # distance between the centre and given point
+
+
+
 size = 50
 res = 1
 dark = True
@@ -25,10 +47,12 @@ if dark == True:
     basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res)+'-dark'
 else:
    basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res) 
-fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass','SubhaloLen','SubhaloMassType','SubhaloSpin','SubhaloVelDisp','SubhaloVmax']
-
+fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass','SubhaloLen','SubhaloMassType','SubhaloSpin',
+          'SubhaloVelDisp','SubhaloVmax','SubhaloBHMdot', 'SubhaloSFR','SubhaloGrNr']
+foffields = ['GroupPos','GroupMass']
 
 subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
+fof_halos = groupcat.loadHalos(basePath,99,fields=foffields)
 num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
 
 a = (groupcat.loadHeader(basePath, snapnum)['Time']).astype('int')  #scalefactor
@@ -36,12 +60,20 @@ z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redsh
 #print(a)
 #print(z)
 
+radial_distance = distancefromcentre(fof_halos['GroupPos'][subhalos['SubhalosGrNr']][0], 
+                                     fof_halos['GroupPos'][subhalos['SubhalosGrNr']][1], 
+                                     fof_halos['GroupPos'][subhalos['SubhalosGrNr']][2], 
+                                     subhalos['SubhaloPos'][:, 0], subhalos['SubhaloPos'][:, 1], 
+                                     subhalos['SubhaloPos'][:, 2])
+
+
 #print(groupcat.loadHeader(basePath, snapnum))
 print(subhalos['SubhaloMassType'])
 
 with open('50-1-subhalo-info-dark.csv', 'w', encoding='UTF8', newline='') as subfile:
     header = ['SubhaloIndex','SubhaloPosX','SubhaloPosY','SubhaloPosZ','SubhaloHalfmassRad','SubhaloMass',
-              'SubhaloLen', 'SubhaloDMMass','SubhaloSpinX','SubhaloSpinY','SubhaloSpinZ','SubhaloVelDisp','SubhaloVmax']
+              'SubhaloLen', 'SubhaloDMMass','SubhaloSpinX','SubhaloSpinY','SubhaloSpinZ','SubhaloVelDisp','SubhaloVmax',
+              'SubhaloBHMdot','SubhaloSFR','FoFMass','FoFDistanceCenter']
     fwriter = csv.writer(subfile, delimiter=',')
     # Write the header
     fwriter.writerow(header)
@@ -49,5 +81,7 @@ with open('50-1-subhalo-info-dark.csv', 'w', encoding='UTF8', newline='') as sub
                       subhalos['SubhaloPos'][:, 2], subhalos['SubhaloHalfmassRad'], 
                       subhalos['SubhaloMass'], subhalos['SubhaloLen'], subhalos['SubhaloMassType'][:, 1], 
                       subhalos['SubhaloSpin'][:, 0],subhalos['SubhaloSpin'][:, 1],subhalos['SubhaloSpin'][:, 2],
-                      subhalos['SubhaloVelDisp'],subhalos['SubhaloVmax']]).transpose()
+                      subhalos['SubhaloVelDisp'],subhalos['SubhaloVmax'],
+                      subhalos['SubhaloBHMdot'],subhalos['SubhaloSFR'], 
+                      fof_halos['GroupMass'][subhalos['SubhalosGrNr']],radial_distance]).transpose()
     fwriter.writerows(data)
