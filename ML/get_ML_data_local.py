482ee237c88d7b2b6bd9337aad18c206657bcc11
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-26 11:25:34 +0100

    Speed up mass binning process

--- ML/get_ML_data_local.py
+++ ML/get_ML_data_local.py
@@ -129,6 +129,6 @@ model_dark = RandomForestRegressor(n_estimators=1000,n_jobs=50)
 model_dark.fit(Xtrain_dark,ytrain_dark)
 y_pred_dark = model_dark.predict(Xtest_dark)
 
-data = pd.DataFrame([Xtest,y_pred,Xtest_dark,y_pred_dark], columns=['Xtest','y_predicted','Xtest_dark','y_predicted_dark'])
+data = pd.DataFrame(np.vstack(Xtest,y_pred,Xtest_dark,y_pred_dark), columns=['Xtest','y_predicted','Xtest_dark','y_predicted_dark'])
 data.to_csv('ML_data')
 
