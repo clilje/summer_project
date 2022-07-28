3baee3acf1538e0c01e7c9fda92f3d97bcbbc1ee
clilje <C.Lilje@sms.ed.ac.uk>
2022-07-28 14:20:24 +0100

    fix matching of DMO

--- ML/predict-concentration-history.py
+++ ML/predict-concentration-history.py
@@ -44,9 +44,11 @@ data_csv = pd.read_csv('50-1-subhalo-history.csv')
 data_csv =data_csv.set_index('index')
 #DMO
 data_csv_dark = pd.read_csv('50-1-subhalo-history-dark.csv')
+
+data_csv_dark['index'] = data_csv_dark['index'].astype(int)
 data_csv_dark = data_csv_dark.set_index('index')
 
-sorter = matchingarr
+sorter = matchingarr.astype(int)
 print(data_csv_dark)
 data_csv_dark_matched = data_csv_dark.reindex([sorter])
 print(data_csv_dark_matched)
@@ -181,7 +183,7 @@ Xtrain_ratio, Xtest_ratio, ytrain_ratio, ytest_ratio = train_test_split(X_ratio,
 fig, axs = plt.subplots(1,3,constrained_layout=True, figsize=(30, 10))
 
 #Train Model for DM+Baryons
-model = RandomForestRegressor(n_estimators=1000,n_jobs=10)
+model = RandomForestRegressor(n_estimators=1000,n_jobs=50)
 model.fit(Xtrain,ytrain)
 y_pred = model.predict(Xtest)
 importances = model.feature_importances_
@@ -201,7 +203,7 @@ axs[0].set_title('Predicted Halo Concentration from Mass Contents, Vmax, VelDisp
 cb = fig.colorbar(im)
 
 #Train Model for DMO
-model_dark = RandomForestRegressor(n_estimators=1000,n_jobs=10)
+model_dark = RandomForestRegressor(n_estimators=1000,n_jobs=50)
 model_dark.fit(Xtrain_dark,ytrain_dark)
 y_pred_dark = model_dark.predict(Xtest_dark)
 importances_dark = model_dark.feature_importances_
@@ -221,7 +223,7 @@ axs[1].set_title('Predicted Halo Concentration from Mass Contents, Vmax, VelDisp
 
 
 
-model_ratio = RandomForestRegressor(n_estimators=1000,n_jobs=10)
+model_ratio = RandomForestRegressor(n_estimators=1000,n_jobs=50)
 model_ratio.fit(Xtrain_ratio,ytrain_ratio)
 y_pred_ratio = model_ratio.predict(Xtest_ratio)
 importances_ratio = model_ratio.feature_importances_
