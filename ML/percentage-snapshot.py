6c4d5647e83c206fb55f6e80bb85510f3c028700
clilje <C.Lilje@sms.ed.ac.uk>
2022-08-04 14:47:51 +0100

    Fix graph and scale

--- ML/percentage-snapshot.py
+++ ML/percentage-snapshot.py
@@ -144,8 +144,9 @@ for x in to_keep:
                       
 FP_number = np.array(FP_number)/number_halos_FP
 DMO_number = np.array(DMO_number)/number_halos_DMO
-plt.loglog(to_keep, FP_number)
-plt.loglog(to_keep, DMO_number)
+plt.plot(to_keep, FP_number, label='Full Physics', marker='.')
+plt.plot(to_keep, DMO_number,label='DMO', marker='.')
+plt.yscale('log')
 plt.xlabel('Snapshot number')
 plt.ylabel('Percentage of halos traced back')
 plt.savefig('halo-percentage')
