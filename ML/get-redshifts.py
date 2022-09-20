57b03d1dc102459ba87ce02a601ce6e8f2e0a819
clilje <C.Lilje@sms.ed.ac.uk>
2022-09-20 22:52:39 +0100

    Get corresponding redshifts

--- ML/get-redshifts.py
+++ ML/get-redshifts.py
@@ -34,8 +34,24 @@ def distancefromcentre(cx, cy, cz, x, y, z, ):
 
     """
     return (np.sqrt((np.power(np.subtract(x,cx), 2)+ np.power(np.subtract(y,cy), 2) + np.power(np.subtract(z,cz), 2)))) # distance between the centre and given point
+"""
+def gcPath(basePath, snapNum, chunkNum=0):
+    """ Return absolute path to a group catalog HDF5 file (modify as needed). """
+    gcPath = basePath + '/fof_subfind_snapshot_%02d/' % snapNum
+    #unsure about filePath1's meaning
+    filePath1 = gcPath + 'fof_subfind_snapshot_%02d.%d.hdf5' % (snapNum, chunkNum)   
+    filePath2 = gcPath + 'fof_subhalo_tab_%03d.%d.hdf5' % (snapNum, chunkNum)
 
+    if isfile(expanduser(filePath1)):
+        return filePath1
+    return filePath2
 
+def loadHeader(basePath, snapNum):
+    """ Load the group catalog header. """
+    with h5py.File(gcPath(basePath, snapNum), 'r') as f:
+        header = dict(f['Header'].attrs.items())
+
+    return header
 
 size = 50
 res = 1
@@ -51,11 +67,17 @@ for i in snapnum:
               'SubhaloSpin','SubhaloVelDisp','SubhaloVmax','SubhaloBHMdot', 'SubhaloSFR','SubhaloGrNr']
     foffields = ['GroupPos','GroupMass']
     
-    subhalos = groupcat.loadSubhalos(basePath,i,fields=fields)
-    fof_halos = groupcat.loadHalos(basePath,i,fields=foffields)
-    num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
+    #subhalos = groupcat.loadSubhalos(basePath,i,fields=fields)
+    #fof_halos = groupcat.loadHalos(basePath,i,fields=foffields)
+    #num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
     
-    a = (groupcat.loadHeader(basePath, i)['Time']).astype('int')  #scalefactor
-    z = (groupcat.loadHeader(basePath, i)['Redshift']).astype('float')  #redshift
+    a = (loadHeader(basePath, i)['Time']).astype('int')  #scalefactor
+    z = (loadHeader(basePath, i)['Redshift']).astype('float')  #redshift
     print(i)
     print(z)
+"""
+
+lhalotree_dir = f'/disk01/rmcg/downloaded/tng/tng50-1/merger_tree/lhalotree/'
+with h5py.File(lhalotree_dir, 'r') as file:
+    redshifts_halos_in_tree = np.array(file['/Header/Redshifts'])
+print(redshifts_halos_in_tree)
