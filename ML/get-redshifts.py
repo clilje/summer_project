6e2cd0e6a16ad06af72a565aae5d763766542549
clilje <C.Lilje@sms.ed.ac.uk>
2022-09-20 20:00:27 +0100

    Get corresponding redshifts

---
+++ ML/get-redshifts.py
@@ -0,0 +1,61 @@
+"""
+Created on Wed Jul  6 22:48:58 2022
+This file was actually used to create the group catalogue file of all halos.
+
+@author: clara
+"""
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import csv
+from pathlib import Path
+import sys
+sys.path.insert(1, '/home/clilje/summer_project')
+from illustris_python import groupcat,lhalotree,snapshot,sublink,util
+
+def distancefromcentre(cx, cy, cz, x, y, z, ):
+    """
+    
+
+    Parameters
+    ----------
+    cx : Halo centre x-coord float
+    cy : Halo centre y-coord float
+    cz : Halo centre z-coord float
+    x : Particle Position x-coord
+    y : Particle Position y-coord
+    z : Particle Position z-coord
+
+    Returns
+    -------
+    Distance between particle and halo centre
+
+    """
+    return (np.sqrt((np.power(np.subtract(x,cx), 2)+ np.power(np.subtract(y,cy), 2) + np.power(np.subtract(z,cz), 2)))) # distance between the centre and given point
+
+
+
+size = 50
+res = 1
+dark = False
+#z = 0
+snapnum = np.arange(9,100,10)
+for i in snapnum:
+    if dark == True:
+        basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res)+'-dark'
+    else:
+       basePath = '/disk01/rmcg/downloaded/tng/tng'+str(size)+'-'+str(res) 
+    fields = ['SubhaloPos','SubhaloHalfmassRad','SubhaloMass','SubhaloLen','SubhaloMassType',
+              'SubhaloSpin','SubhaloVelDisp','SubhaloVmax','SubhaloBHMdot', 'SubhaloSFR','SubhaloGrNr']
+    foffields = ['GroupPos','GroupMass']
+    
+    subhalos = groupcat.loadSubhalos(basePath,99,fields=fields)
+    fof_halos = groupcat.loadHalos(basePath,99,fields=foffields)
+    num_halo = np.arange(len(np.array(subhalos['SubhaloMass'])))
+    
+    a = (groupcat.loadHeader(basePath, snapnum)['Time']).astype('int')  #scalefactor
+    z = (groupcat.loadHeader(basePath, snapnum)['Redshift']).astype('float')  #redshift
+    print(i)
+    print(z)
