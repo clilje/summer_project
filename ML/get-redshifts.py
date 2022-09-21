8ab86fe2e26dc9df4624dda6a5063d5e3e060f23
clilje <C.Lilje@sms.ed.ac.uk>
2022-09-21 08:56:56 +0100

    Get corresponding redshifts

--- ML/get-redshifts.py
+++ ML/get-redshifts.py
@@ -36,7 +36,9 @@ def distancefromcentre(cx, cy, cz, x, y, z, ):
     return (np.sqrt((np.power(np.subtract(x,cx), 2)+ np.power(np.subtract(y,cy), 2) + np.power(np.subtract(z,cz), 2)))) # distance between the centre and given point
 """
 def gcPath(basePath, snapNum, chunkNum=0):
-    """ Return absolute path to a group catalog HDF5 file (modify as needed). """
+    """ 
+#Return absolute path to a group catalog HDF5 file (modify as needed). 
+"""
     gcPath = basePath + '/fof_subfind_snapshot_%02d/' % snapNum
     #unsure about filePath1's meaning
     filePath1 = gcPath + 'fof_subfind_snapshot_%02d.%d.hdf5' % (snapNum, chunkNum)   
@@ -47,7 +49,9 @@ def gcPath(basePath, snapNum, chunkNum=0):
     return filePath2
 
 def loadHeader(basePath, snapNum):
-    """ Load the group catalog header. """
+    """ 
+#Load the group catalog header. 
+"""
     with h5py.File(gcPath(basePath, snapNum), 'r') as f:
         header = dict(f['Header'].attrs.items())
 
@@ -77,7 +81,7 @@ for i in snapnum:
     print(z)
 """
 
-lhalotree_dir = f'/disk01/rmcg/downloaded/tng/tng50-1/merger_tree/lhalotree/'
+lhalotree_dir = '/disk01/rmcg/downloaded/tng/tng50-1/merger_tree/lhalotree/'
 with h5py.File(lhalotree_dir, 'r') as file:
     redshifts_halos_in_tree = np.array(file['/Header/Redshifts'])
 print(redshifts_halos_in_tree)
